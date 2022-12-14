63d1ef1c120299b1e49a9a9bfe6ea301632af663
NicoleLatifi <Nicole.Latifi@gmail.com>
2022-12-13 21:54:09 -0700

    feat: add in background images

--- src/App.js
+++ src/App.js
@@ -3,35 +3,43 @@ import "./categories.styles.scss";
 const App = () => {
   const categories = [
     {
-      id: "1",
-      title: "Hats",
+      id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
-      id: "2",
-      title: "Jackets",
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
-      id: "3",
-      title: "Sneakers",
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
-      id: "4",
-      title: "Womens",
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
-      id: "5",
-      title: "Mens",
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
 
   return (
     <div className="categories-container">
-      {categories.map((category) => {
+      {categories.map(({ id, imageUrl, title }) => {
         return (
-          <div className="category" key={category.id}>
-            <div className="background-image" />
+          <div className="category" key={id}>
+            <div
+              className="background-image"
+              style={{ backgroundImage: `url(${imageUrl})` }}
+            />
             <div className="category-body">
-              <h2>{category.title}</h2>
+              <h2>{title}</h2>
               <p>Shop Now</p>
             </div>
           </div>
