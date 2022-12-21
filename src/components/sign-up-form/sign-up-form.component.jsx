9c5d5565fb9b60001f1f241b05b1249d39274c4d
NicoleLatifi <Nicole.Latifi@gmail.com>
2022-12-21 09:28:18 -0700

    feat: create Button component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -5,6 +5,7 @@ import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
+import Button from "../button/button.component";
 import FormInput from "../form-input/form-input.component";
 
 const defaultFormFields = {
@@ -99,7 +100,7 @@ const SignUpForm = () => {
             value: confirmPassword,
           }}
         />
-        <button type="submit">Sign Up</button>
+        <Button type="submit">Sign Up</Button>
       </form>
     </div>
   );
