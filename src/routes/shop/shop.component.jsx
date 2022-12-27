7ec27d6f8c95300746ac924c228a02f0272c2e79
NicoleLatifi <Nicole.Latifi@gmail.com>
2022-12-27 12:10:16 -0700

    feat: create ProductCard

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,19 +1,17 @@
 import { useContext } from "react";
+
+import "./shop.styles.scss";
 import { ProductsContext } from "../../context/products.context";
+import ProductCard from "../../components/product-card/product-card.component";
 
 const Shop = () => {
   const { products } = useContext(ProductsContext);
 
   return (
-    <div>
+    <div className="products-container">
       {products &&
         products.map((product) => {
-          return (
-            <div key={product.id}>
-              <p>{product.name}</p>
-              <p>${product.price}</p>
-            </div>
-          );
+          return <ProductCard key={product.id} product={product} />;
         })}
     </div>
   );
